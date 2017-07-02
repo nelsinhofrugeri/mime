8957857ed4b6346c84b89a8122975ae47d676c43
nelsinho.frugeri <nfrugeri@gmail.com>
2017-07-01 22:22:42 -0300

    Implemented method POST for client query

--- src/apiai/client/handler.js
+++ src/apiai/client/handler.js
@@ -1,18 +1,52 @@
 'use strict';
 
 const requestPromise = require('request-promise');
+const HEADERS = { 'Authorization': `Bearer ${process.env.APIAI_CLIENT_TOKEN}` };
+const ISJSON = true;
 
 class Handler {
+    post (request, reply) {
+        let timezone = new Date();
+        let payload = {
+            "query": [
+                "and for tomorrow"
+            ],
+            "contexts": [{
+                "name": "weather",
+                "lifespan": 4
+            }],
+            "location": {
+                "latitude": 37.459157,
+                "longitude": -122.17926
+            },
+            "timezone": "America/New_York",
+            "lang": "en",
+            "sessionId": "1234567890"
+        };
+        let options = {
+            method: 'POST',
+            uri: 'https://api.api.ai/v1/query?v=20150910',
+            headers: HEADERS,
+            body: payload,
+            json: ISJSON
+        };
+
+        requestPromise(options)
+            .then((result) => {
+                return reply({'query': result});
+            })
+            .catch((err) => {
+                return reply({'error': err});
+            })
+    }
+
     get (request, reply) {
         let timezone = new Date();
         let query = request.query.query;
-
         let options = {
             uri: `https://api.api.ai/api/query?v=20150910&query=${query}&lang=pt-br&sessionId=${process.env.APIAI_SESSION_ID}&timezone=${timezone}`,
-            headers: {
-                'Authorization': `Bearer ${process.env.APIAI_CLIENT_TOKEN}`
-            },
-            json: true
+            headers: HEADERS,
+            json: ISJSON
         };
 
         requestPromise(options)
@@ -21,7 +55,7 @@ class Handler {
             })
             .catch((err) => {
 
-                return reply({'err': err});
+                return reply({'error': err});
             });
     }
 }
--- src/apiai/client/routes.js
+++ src/apiai/client/routes.js
@@ -8,5 +8,10 @@ module.exports = [
         method: 'GET',
         path: '/v1/apiai/client',
         handler: handler.get
+    },
+    {
+        method: 'POST',
+        path: '/v1/apiai/client',
+        handler: handler.post
     }
 ]
