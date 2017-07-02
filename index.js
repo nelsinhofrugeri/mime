16a9e33e8592d740dc4f1d45be61d3046e34c746
nelsinho.frugeri <nfrugeri@gmail.com>
2017-07-01 21:07:12 -0300

    Implemented route to get intents

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const Hapi = require('hapi');
 const hapiRouter = require('hapi-router');
 
--- src/api/ai/handler.js
+++ src/api/ai/handler.js
@@ -1,11 +1,25 @@
 'use strict';
 
+const requestPromise = require('request-promise');
+
 class Handler {
-    get () {
-        console.log('test');
-        return {
-            'test': 'test!'
-        }
+    get (request, reply) {
+        let options = {
+            uri: 'https://api.api.ai/v1/intents?v=20150910',
+            headers: {
+                'Authorization': `Bearer ${process.env.APIAI_TOKEN}`
+            },
+            json: true
+        };
+
+        requestPromise(options)
+            .then((intents) => {
+                return reply({'intents': intents})
+            })
+            .catch((err) => {
+
+                return reply({'err': err});
+            });
     }
 }
 
